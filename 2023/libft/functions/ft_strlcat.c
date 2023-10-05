dd87b7118c6e471ca92ad40fc36afd733050d350
lureb74 <140264556+lureb74@users.noreply.github.com>
2023-10-05 09:52:49 +0200

    modif strlcat

--- 2023/libft/functions/ft_strlcat.c
+++ 2023/libft/functions/ft_strlcat.c
@@ -10,29 +10,55 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-//#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
 
-char *ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlen(const char *str)
 {
-	size_t	d;
-	size_t	s;
+	size_t	i;
 
-	d = 0;
-	s = 0;
-	while(dest[d])
-		d++;
-	while(src[s])
-		dest[d++] = src[s++];
-	dest[d] = 0;
-	return (dest);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
-/*
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t		dst_c;
+	size_t		s_len;
+	size_t		i;
+
+	if (size == 0) // si può scrivere anche if(!size)
+		return (ft_strlen(dst));
+	s_len = ft_strlen(src);
+	i = 0;
+	while (dst[i] && i < size)
+		i++;
+	dst_c = i;
+	while (src[i - dst_c] && i < size - 1)
+	{
+		dst[i] = src[i - dst_c];
+		i++;
+	}
+	if (dst_c < size)
+		dst[i] = '\0';
+	return (dst_c + s_len);
+}
+
 int	main(void)
 {
-	char dest[15] = "qwerty";
+	char		dst[] = "questa stringa ";
+	const char	src[] = "e' stata copiata per intero";
+	size_t		r;
+	size_t		size = 43;
 
-	ft_strlcat(dest, " uiop");
-	printf("%s\n", dest);
+	r = ft_strlcat(dst, src, size);
+	printf("Value: %ld\n", r);
+	puts(dst);
+	if(r >= size)
+		puts("Stringa interrotta");
+	else
+		puts("Stringa completa");
 	return (0);
-}*/
+}
