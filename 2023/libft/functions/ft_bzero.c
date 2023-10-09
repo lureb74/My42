b2d935b5e4670be88154752a6a55bdcbc000d379
lureb74 <140264556+lureb74@users.noreply.github.com>
2023-10-10 00:33:07 +0200

    added bzero e memset

---
+++ 2023/libft/functions/ft_bzero.c
@@ -0,0 +1,23 @@
+//#include <stdio.h>
+#include <string.h>
+
+void	ft_bzero(void *s, size_t n)
+{
+	unsigned char	*us;
+	size_t	i;
+
+	us = s;
+	i = 0;
+	while (i < n)
+		us[i++] = '\0';
+}
+/*
+int	main(void)
+{
+	char	s[] = "qwertyuiop";
+	size_t	n = 1;
+
+	ft_bzero(s, n);
+	printf("%s\n", s);
+	return (0);
+}*/
---
+++ 2023/libft/functions/ft_memset.c
@@ -0,0 +1,25 @@
+//#include <stdio.h>
+#include <string.h>
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*us;
+	size_t	i;
+
+	us = s;
+	i = 0;
+	while (i < n)
+		us[i++] = c;
+	return (s);
+}
+/*
+int	main(void)
+{
+	char	s[] = "qwertyuiop";
+	int		c = 120;
+	size_t	n = 6;
+
+	ft_memset(s, c, n);
+	printf("%s\n", s);
+	return (0);
+}*/
