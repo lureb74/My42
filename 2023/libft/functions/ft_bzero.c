153d80ee35daa130fe5297ac0967370f406064d4
lureb74 <140264556+lureb74@users.noreply.github.com>
2023-10-14 21:21:28 +0200

    Update ft_bzero.c

--- 2023/libft/functions/ft_bzero.c
+++ 2023/libft/functions/ft_bzero.c
@@ -1,10 +1,10 @@
 //#include <stdio.h>
-#include <string.h>
+//#include <string.h>
 
 void	ft_bzero(void *s, size_t n)
 {
 	unsigned char	*us;
-	size_t		i;
+	size_t			i;
 
 	us = (unsigned char *)s;
 	i = 0;
@@ -18,6 +18,16 @@ int	main(void)
 	size_t	n = 1;
 
 	ft_bzero(s, n);
+	printf("%s\n", s);
+	return (0);
+}*/
+/*
+int	main(void)
+{
+	char	s[] = "qwertyuiop";
+	size_t	n = 1;
+
+	bzero(s, n);
 	printf("%s\n", s);
 	return (0);
 }*/
