1accda5f8c059d0ce21823b2e9c8db6ea3ee5bfd
lureb74 <140264556+lureb74@users.noreply.github.com>
2023-10-14 13:51:26 +0200

    Create ft_strlcpy.c

---
+++ 2023/libft/functions/ft_strlcpy.c
@@ -0,0 +1,63 @@
+//#include "libft.h"
+//#include <bsd/string.h>
+//#include <stdio.h>
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t		s_len;
+	size_t		i;
+
+	s_len = ft_strlen(src);
+	i = 0;
+	while (src[i] && i < size - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (s_len);
+}
+/*
+int	main(void)
+{
+	char		dst[] = "";
+	const char	src[] = "Questa stringa e' stata copiata per intero";
+	size_t		r;
+	size_t		size = 31;
+
+	r = ft_strlcpy(dst, src, size);
+	printf("FT:\nValue: %ld\n", r);
+	puts(dst);
+	if(r >= size)
+		puts("Stringa interrotta");
+	else
+		puts("Stringa completa");
+	return (0);
+}*/
+/*
+int	main(void)
+{
+	char		dst[] = "";
+	const char	src[] = "Questa stringa e' stata copiata per intero";
+	size_t		r;
+	size_t		size = 31;
+
+	r = strlcpy(dst, src, size);
+	printf("ORIGINAL:\nValue: %ld\n", r);
+	puts(dst);
+	if(r >= size)
+		puts("Stringa interrotta");
+	else
+		puts("Stringa completa");
+	return (0);
+}*/
