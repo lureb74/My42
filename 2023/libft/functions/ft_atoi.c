a8c36e4a0815300100e527ed57743202551b800c
lureb74 <140264556+lureb74@users.noreply.github.com>
2023-10-14 21:17:10 +0200

    Update ft_atoi.c

--- 2023/libft/functions/ft_atoi.c
+++ 2023/libft/functions/ft_atoi.c
@@ -1,19 +1,16 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: lobartol <lobartol@student.42firenze.it>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/25 08:23:30 by lobartol          #+#    #+#             */
-/*   Updated: 2023/09/25 08:23:42 by lobartol         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 //#include "libft.h"
-#include<string.h>
+//#include<stdlib.h>
 //#include <stdio.h>
 
+/*
+ASCII values "syntax in strings" and names for ISSPACE:
+9	"\t" horizontal tab
+10	"\n" line feed (new line)
+11	"\v" vertical tab
+12	"\f" form feed
+13	"\r" carriage return
+32	" "  space
+*/
 int	ft_atoi(const char *str)
 {
 	size_t	i;
@@ -23,20 +20,12 @@ int	ft_atoi(const char *str)
 	i = 0;
 	c = 0;
 	s = 1;
-//	ASCII values "syntax in strings" and names for ISSPACE:
-//	9	"\t" horizontal tab
-//	10	"\n" line feed (new line)
-//	11	"\v" vertical tab
-//	12	"\f" form feed
-//	13	"\r" carriage return
-//	32	" "  space
 	if (*str == 0)
 		return (0);
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
-//		s = (str[c] == '-' ? -1 : 1);
 		if (str[i] == '-')
 			s = -1;
 		i++;
@@ -51,8 +40,12 @@ int	ft_atoi(const char *str)
 /*
 int	main(void)
 {
-	int		a;
+	const char	str[] = "	\v\n\f\t\r -00021x4362";
+	int			ift;
+	int			ior;
 
-	a = ft_atoi(" \t\v\n\f\r \t\r 		-00021x4362");
-	printf("%d\n", a);
+	ift = ft_atoi(str);
+	printf("FT:\n%d\n", ift);
+	ior = atoi(str);
+	printf("ORIGINAL:\n%d\n", ior);
 }*/
