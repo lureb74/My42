6765eb1c4aa433c0e192f4ff12669dfa28986e17
lureb74 <140264556+lureb74@users.noreply.github.com>
2023-10-18 09:58:17 +0200

    Update ft_calloc.c

--- 2023/libft/functions/ft_calloc.c
+++ 2023/libft/functions/ft_calloc.c
@@ -1,9 +1,21 @@
 //#include "libft.h"
-#include <stdlib.h>
+//#include <stdlib.h>
+//#include <stdio.h>
+
+void	ft_bzero(void *s, size_t n)
+{
+	unsigned char	*us;
+	size_t			i;
+
+	us = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+		us[i++] = '\0';
+}
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	void	*ptr
+	void	*ptr;
 
 	ptr = malloc(nmemb * size);
 	if (!ptr)
@@ -11,3 +23,16 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	ft_bzero(ptr, nmemb * size);
 	return (ptr);	
 }
+/*
+int	main(void)
+{
+	void		*ptr;
+	size_t		nmemb = 3;
+	size_t		size = 2;
+	char		*p;
+
+	ptr = ft_calloc(nmemb, size);
+	p = ptr;
+	printf("%s\n", p);
+	return (0);
+}*/
