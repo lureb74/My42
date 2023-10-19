1e4af621fbecc7c13678078d14f2034624aff057
lureb74 <140264556+lureb74@users.noreply.github.com>
2023-10-19 18:15:53 +0200

    Update ft_memset.c

--- 2023/libft/functions/ft_memset.c
+++ 2023/libft/functions/ft_memset.c
@@ -3,20 +3,15 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	unsigned char	*us;
-	size_t		i;
-
-	us = (unsigned char *)s;
-	i = 0;
-	while (i < n)
-		us[i++] = (unsigned char)c;
+	while (n--)
+		*(unsigned char *)s++ = (unsigned char)c;
 	return (s);
 }
 /*
 int	main(void)
 {
 	char	s[] = "qwertyuiop";
-	int	c = 120;
+	int		c = 120;
 	size_t	n = 6;
 
 	ft_memset(s, c, n);
