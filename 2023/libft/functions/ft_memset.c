7723f6563fe27229327c7f790639a1e9a56a9f63
lureb74 <140264556+lureb74@users.noreply.github.com>
2023-10-20 18:50:50 +0200

    Update ft_memset.c

--- 2023/libft/functions/ft_memset.c
+++ 2023/libft/functions/ft_memset.c
@@ -1,6 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lobartol <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 18:25:03 by lobartol          #+#    #+#             */
+/*   Updated: 2023/10/09 18:25:07 by lobartol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 //#include <stdio.h>
 //#include <string.h>
+/*
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*us;
+	size_t			i;
 
+	us = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+		us[i++] = (unsigned char)c;
+	return (s);
+}
+*/
 void	*ft_memset(void *s, int c, size_t n)
 {
 	while (n--)
@@ -10,11 +34,14 @@ void	*ft_memset(void *s, int c, size_t n)
 /*
 int	main(void)
 {
-	char	s[] = "qwertyuiop";
+	char	sft[] = "qwertyuiop";
+	char	sor[] = "qwertyuiop";
 	int		c = 120;
 	size_t	n = 6;
 
-	ft_memset(s, c, n);
-	printf("%s\n", s);
+	ft_memset(sft, c, n);
+	printf("FT:\n%s\n", sft);
+	memset(sor, c, n);
+	printf("ORIGINAL:\n%s\n", sor);
 	return (0);
 }*/
