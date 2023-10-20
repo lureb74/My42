371a02051d6a28f611e1bac1f9bb2e05ae7242fa
lureb74 <140264556+lureb74@users.noreply.github.com>
2023-10-20 18:24:15 +0200

    Update ft_memcmp.c

--- 2023/libft/functions/ft_memcmp.c
+++ 2023/libft/functions/ft_memcmp.c
@@ -12,7 +12,24 @@
 
 //#include <stdio.h>
 //#include <string.h>
+/*
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*alts1;
+	unsigned char	*alts2;
 
+	alts1 = (unsigned char *)s1;
+	alts2 = (unsigned char *)s2;
+	if (n == 0)
+		return (0);
+	while (*alts1 == *alts2 && *alts1 && *alts1 < n - 1)
+	{
+		alts1++;
+		alts2++;
+	}
+	return (*alts1 - *alts2);
+}
+*/
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t	i;
