acc787854200c583d94fedab7a9163eaab3452b3
lureb74 <140264556+lureb74@users.noreply.github.com>
2023-10-20 18:34:23 +0200

    Update ft_memcpy.c

--- 2023/libft/functions/ft_memcpy.c
+++ 2023/libft/functions/ft_memcpy.c
@@ -1,20 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lobartol <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 18:42:41 by lobartol          #+#    #+#             */
+/*   Updated: 2023/10/11 18:42:47 by lobartol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 //#include <stdio.h>
 //#include <string.h>
+/*
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	while (n--)
+		*d++ = *s++;
+	*d = '\0';
+	return (dest);
+}*/
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	while (n--)
 		*(unsigned char *)dest++ = *(unsigned char *)src++;
+	*(unsigned char *)dest = '\0';
 	return (dest);
 }
 /*
 int	main(void)
 {
-	char		dest[15];
+	char		dest[11];
 	const char	src[] = "qwertyuiop";
-	size_t		n = 6;
+	char		dst[11];
+	const char	sr[] = "qwertyuiop";
+	size_t		n = 20;
 
 	ft_memcpy(dest, src, n);
-	printf("%s\n", dest);
+	printf("FT:\n%s\n", dest);
+	memcpy(dst, sr, n);
+	printf("ORIGINAL:\n%s\n", dst);
 	return (0);
 }*/
