1a46826681778c53d190e18d4d4a2c5ebb018d99
lureb74 <140264556+lureb74@users.noreply.github.com>
2023-10-21 16:00:35 +0200

    Update ft_substr.c

--- 2023/libft/additional functions/ft_substr.c
+++ 2023/libft/additional functions/ft_substr.c
@@ -11,8 +11,8 @@
 /* ************************************************************************** */
 
 //#include "libft.h"
-//#include <stdlib.h>
 //#include <stdio.h>
+#include <stdlib.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
