8865c1c074b51d71313a15cc877c80c17e53caab
lureb74 <140264556+lureb74@users.noreply.github.com>
2023-10-23 19:51:03 +0200

    Update ft_itoa.c

--- 2023/libft/additional functions/ft_itoa.c
+++ 2023/libft/additional functions/ft_itoa.c
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lobartol <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/23 18:07:19 by lobartol          #+#    #+#             */
+/*   Updated: 2023/10/23 18:07:24 by lobartol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 static int	count(int n)
 {
 	int	c;
 
 	c = 0;
-	if (n == 0)
-		return (1);
+	if (n <= 0)
+		c = 1;
 	while (n)
 	{
 		n /= 10;
@@ -18,38 +30,38 @@ static int	count(int n)
 
 char	*ft_itoa(int n)
 {
-	int		ncopy;
-	int		ncount;
+	long	ncopy;
+	int		len;
 	char	*r;
 
-	ncount = count(n);
-//	while (ncount && ++c < 10)
-//		ncount /= 10;
-	printf("%d\n", ncount);
-	if (n < 0)
-		ncount++;
-	r = (char *)malloc((ncount + 1) * sizeof(char));
+	ncopy = n;
+	len = count(n);
+	r = (char *)malloc((len + 1) * sizeof(char));
 	if (!r)
 		return (NULL);
-	ncopy = n;
-//	r[ncount] = '\0';
-	while (--ncount >= 0)
+	r[len] = '\0';
+	if (ncopy < 0)
 	{
-		r[ncount] = ncopy % 10 + 48;
-		ncopy /= 10;
+		r[0] = '-';
+		ncopy *= -1;
 	}
-	if (n < 0)
-		r[ncount] = '-';
-	r[count(n)] = '\0';
+	if (ncopy == 0)
+		r[0] = '0';
+	while (ncopy != 0)
+	{
+		r[--len] = (ncopy % 10) + 48;
+		ncopy = ncopy / 10;
+	}
 	return (r);
 }
-
-int main(void)
+/*
+int	main(void)
 {
-	int		i = 2147483647;
+	int		i = -2147483648;
 	char    *r;
 
 	r = ft_itoa(i);
 	printf("%s\n", r);
+	free(r);
 	return (0);
-}
+}*/
