7a6bcc0959a3cfd03730a8cc772fb2530504f8dc
lureb74 <140264556+lureb74@users.noreply.github.com>
2023-10-24 22:53:57 +0200

    Add files via upload

---
+++ 2023/libft/additional functions/ft_putnbr_fd.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lobartol <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 19:31:02 by lobartol          #+#    #+#             */
+/*   Updated: 2023/10/24 19:31:06 by lobartol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	count(int n)
+{
+	int	c;
+
+	c = 0;
+	if (n <= 0)
+		c = 1;
+	while (n)
+	{
+		n /= 10;
+		c++;
+	}
+	return (c);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s || !fd)
+		return ;
+	write(fd, s, ft_strlen(s));
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long	ncopy;
+	int		len;
+	char	*r;
+
+	ncopy = n;
+	len = count(n);
+	if (ncopy < 0)
+	{
+		r[0] = '-';
+		ncopy *= -1;
+	}
+	if (ncopy == 0)
+		r[0] = '0';
+	while (ncopy != 0)
+	{
+		r[--len] = ncopy % 10 + 48;
+		ncopy /= 10;
+	}
+	r[ncount(n)] = '\0';
+	ft_putstr_fd(r, fd);
+}
---
+++ 2023/libft/additional functions/ft_strmapi.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lobartol <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 18:16:35 by lobartol          #+#    #+#             */
+/*   Updated: 2023/10/24 18:17:05 by lobartol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include "libft.h"
+//#include <stdio.h>
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*r;
+	size_t	i;
+
+	if (!s || !f)
+		return (NULL);
+	r = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (!r)
+		return (NULL);
+	i = 0;
+	while (s[i])
+		{
+			r[i] = (*f)(i, s[i]);
+			i++;
+		}
+		r[i] = '\0';
+		return (r);
+}
