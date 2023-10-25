a1dc09cea9268d010580d7be7644d08193cc5502
lureb74 <140264556+lureb74@users.noreply.github.com>
2023-10-25 19:40:19 +0200

    Update ft_split.c

--- 2023/libft/additional functions/ft_split.c
+++ 2023/libft/additional functions/ft_split.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 //#include <stdio.h>
-#include <stdlib.h>
+#include "libft.h"
 
 static int	lines(char const *s, char c)
 {
