b113b25dc1624b2de4c14af749345f506a1c0f48
lureb74 <140264556+lureb74@users.noreply.github.com>
2023-10-25 19:45:58 +0200

    Update ft_strlcat.c

--- 2023/libft/functions/ft_strlcat.c
+++ 2023/libft/functions/ft_strlcat.c
@@ -10,19 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "libft.h"
 //#include <stdio.h>
 //#include <bsd/string.h>
-
-size_t	ft_strlen(const char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
+#include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
