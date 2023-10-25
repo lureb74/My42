d3317c447dda294930f085f6093214dd4f272f20
lureb74 <140264556+lureb74@users.noreply.github.com>
2023-10-25 19:44:25 +0200

    Update ft_strjoin.c

--- 2023/libft/additional functions/ft_strjoin.c
+++ 2023/libft/additional functions/ft_strjoin.c
@@ -10,9 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include <stdlib.h>
 //#include <stdio.h>
+#include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
