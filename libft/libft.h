4a944161369cdaf88cd38ab05dd78fff64775344
lureb74 <140264556+lureb74@users.noreply.github.com>
2023-11-03 21:10:55 +0100

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -52,7 +52,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 
-typedef struct	s_list
+typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
