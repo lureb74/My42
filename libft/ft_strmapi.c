0e785178a86857fda3e0090aed7308c3f54f7cb7
lureb74 <140264556+lureb74@users.noreply.github.com>
2023-11-03 21:11:56 +0100

    Update ft_strmapi.c

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -24,10 +24,10 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 		return (NULL);
 	i = 0;
 	while (s[i])
-		{
-			r[i] = (*f)(i, s[i]);
-			i++;
-		}
-		r[i] = '\0';
-		return (r);
+	{
+		r[i] = (*f)(i, s[i]);
+		i++;
+	}
+	r[i] = '\0';
+	return (r);
 }
