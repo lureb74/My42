295372b0ae0b2683e4749fe0a14f194b33c4f74e
lureb74 <140264556+lureb74@users.noreply.github.com>
2023-11-04 16:51:03 +0100

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -13,9 +13,10 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <unistd.h>
-# include <stdlib.h>
+# include <limits.h>
 # include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
@@ -67,5 +68,4 @@ t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 t_list	*ft_lstnew(void *content);
 int		ft_lstsize(t_list *lst);
-
 #endif
